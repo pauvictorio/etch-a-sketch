85d256afd2c00a139efff259c345e540264efd8a
Paulo Victorio <paulo_victorio@yahoo.com>
2022-08-16 06:48:23 +0800

    Refactor whole JS script, small revisions to HTML and CSS files

--- script.js
+++ script.js
@@ -1,55 +1,114 @@
-const slider = document.querySelector('.slider');
-const text = document.querySelector('.text');
-const etch = document.querySelector('.etch');
+// Grid
+const grid = document.querySelector('.grid');
 
-function debounce(func, wait = 20, immediate = true) {
-  var timeout;
-  return function() {
-    var context = this, args = arguments;
-    var later = function() {
-      timeout = null;
-      if (!immediate) func.apply(context, args);
-    };
-    var callNow = immediate && !timeout;
-    clearTimeout(timeout);
-    timeout = setTimeout(later, wait);
-    if (callNow) func.apply(context, args);
-  };
-};
+let color = 'black';
+let rgb = false;
 
-function changeDivSize() {
-  const canvs = document.querySelectorAll('.canv');
 
-  canvs.forEach(canv => {
-    canv.style.width = `${1 / slider.value * 100}%`;
-    canv.style.height = `${1 / slider.value * 100}%`;
-  });
+// Handle RGB
+function createRGB() {
+  const randomBetween = (min, max) => min + Math.floor(Math.random() * (max - min + 1));
+
+  // Assign random values to R, G, B
+  const r = randomBetween(0, 255);
+  const g = randomBetween(0, 255);
+  const b = randomBetween(0, 255);
+
+  rgb = true;
+
+  return `rgb(${r}, ${g}, ${b})`;
 }
 
-function generateDivs(numDivs) {
-  etch.innerHTML = '';
-  for (let i = 1; i <= numDivs; i++) {
-    for (let j = 1; j <= numDivs; j++) {
-      etch.innerHTML += `<div class="canv"></div>`;
-    } 
+
+// Handle grid creation
+function createGrid(size) {
+  const activateGrid = document.querySelector('.btn-grid-activate');
+
+  // Restart grid whenever createGrid is called
+  while (grid.firstChild) {
+    grid.innerHTML = '';
   }
-  changeDivSize();
+
+  // Loop size number of times (from slider value)
+  for (let i = 1; i <= size * size; i++) {
+
+    // Create div inside grid
+    const gridElement = document.createElement('div');
+
+    gridElement.classList.add('grid-element');
+
+    // Change size of grid-elements
+    gridElement.style.height = `${1 / size * 100}%`;
+    gridElement.style.width = `${1 / size * 100}%`;
+
+    grid.appendChild(gridElement);
+
+    // Change grid-elements' color on mouseover event
+    gridElement.addEventListener('mouseover', () => {
+      if (rgb) gridElement.style.background = createRGB();
+
+      else { gridElement.style.background = color; }
+    });
+
+    // Toggle grid border
+    activateGrid.addEventListener('click', () => {
+      gridElement.classList.toggle('grid-active')
+    });
+  }
 }
 
 
-function slideSizer() {
+// Menu
+const clearButton = document.querySelector('.clear');
+const colorPicker = document.querySelector('#color');
+const toggleRGB = document.querySelector('.rgb');
+const slider = document.querySelector('.slider');
+const sliderText = document.querySelector('.slider-text');
+
+let mousedown = false;
+
+// Handle range slider
+function sliderSize() {
+  // Change value of slider
   this.setAttribute('value', this.value);
-  text.textContent = `${this.value} x ${this.value}`;
 
-  generateDivs(this.value);  
+  // Change text inside to indicate the size
+  sliderText.textContent = `${this.value} x ${this.value}`;
+
+  if (mousedown) {
+    createGrid(this.value);
+  }
 }
 
 
-slider.addEventListener('click', slideSizer);
-slider.addEventListener('mousemove', debounce(slideSizer));
+// Change color on input
+colorPicker.addEventListener('input', () => {
+  // Turn off RGB
+  rgb = false;
+
+  color = colorPicker.value;
+})
 
 
+// Toggle RGB
+toggleRGB.addEventListener('click', createRGB);
+
+
+// Clear the grid's color
+clearButton.addEventListener('click', e => {
+  createGrid(slider.value);
+});
+
+
+// Add event listeners to range slider
+slider.addEventListener('click', sliderSize);
+slider.addEventListener('mousedown', () => mousedown = true);
+slider.addEventListener('mouseup', () => mousedown = false);
+slider.addEventListener('mousemove', sliderSize);
+
+
+// Load the grid
 window.addEventListener('load', () => {
-  text.textContent = `${slider.value} x ${slider.value}`;
-  generateDivs(slider.value);
+  sliderText.textContent = `${slider.value} x ${slider.value}`;
+  createGrid(slider.value);
 });
